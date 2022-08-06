98de0703219223a3f883d88986c8195c36f1937e
adut24 <dutertre.alexandre@laposte.net>
2022-08-06 08:47:51 +0000

    Setup sql script

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Query to create a new database named hbnb_test_db
+-- Query to create a new user named hbnb_test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
