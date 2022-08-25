5fb63b50e4d99f474bdbee4db9e79e23272c8efa
Nicolas Boute <4249@holbertonschool.com>
2022-08-25 15:19:38 +0200

    trying with my version of the script now that the error was found

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,45 +27,28 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Deploy given archive to web server"""
-
     if os.path.exists(archive_path) is False:
         return False
-
     filename = os.path.basename(archive_path)
     result = put(archive_path, '/tmp/{}'.format(filename))
     if result.failed is True:
         return False
-
     result = run("mkdir -p /data/web_static/releases/{}/"
                  .format(filename[:-4]))
     if result.failed is True:
         return False
-
-    result = run("tar -xzf /tmp/" +
-                 "{} -C /data/web_static/releases/{}/"
-                 .format(filename, filename[:-4]))
+    result = run(("tar -xzf /tmp/" +
+                  "{} -C /data/web_static/releases/{}/" +
+                  " --strip-components=1").format(filename, filename[:-4]))
     if result.failed is True:
         return False
-
     result = run("rm /tmp/{}".format(filename))
     if result.failed is True:
         return False
-
-    result = run("mv /data/web_static/releases/" +
-                 "{0}/web_static/* /data/web_static/releases/{0}/"
-                 .format(filename[:-4:]))
-    if result.failed is True:
-        return False
-
-    result = run("rm -rf /data/web_static/releases/{}/web_static"
-                 .format(filename[:-4]))
-    if result.failed is True:
-        return False
-
     result = run("rm -rf /data/web_static/current")
     if result.failed is True:
         return False
-    result = run("ln -s /data/web_static/releases/" +
+    result = run("ln -s /data/web_static/releases/"
                  "{}/ /data/web_static/current".format(filename[:-4]))
     if result.failed is True:
         return False
