47dcd1a93048c3d3945a913fef5adce652b055e3
Nicolas Boute <4249@holbertonschool.com>
2022-08-25 15:56:36 +0200

    added puppet script

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,11 @@
+exec {'/usr/bin/env apt update': }
+-> exec {'/usr/bin/env apt -y install nginx': }
+-> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test': }
+-> exec {'/usr/bin/env mkdir -p /data/web_static/shared': }
+-> exec {'/usr/bin/env echo "This is a website" >> /data/web_static/releases/test/index.html': }
+-> exec {'/usr/bin/env rm -rf /data/web_static/current': }
+-> exec {'/usr/bin/env ln -sT /data/web_static/releases/test/ /data/web_static/current': }
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data/': }
+-> exec {'/usr/bin/env sed -i "47i location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default': }
+-> exec {'/usr/bin/env echo "Configuration executed successfully"': }
+-> exec {'/usr/bin/env service nginx restart': }
