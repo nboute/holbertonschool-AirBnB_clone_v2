931526217263035b27ad9d4b06c9660069f99a37
Nicolas Boute <4249@holbertonschool.com>
2022-08-25 15:23:40 +0200

    fixed subsequent versions of the script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -31,29 +31,32 @@ def do_deploy(archive_path):
     """Deploy given archive to web server"""
     if os.path.exists(archive_path) is False:
         return False
-    result = put(archive_path, '/tmp/')
-    if result.failed is True:
-        return False
     filename = os.path.basename(archive_path)
-    result = run("mkdir -p /data/web_static/releases/{}".format(filename[:-4]))
+    result = put(archive_path, '/tmp/{}'.format(filename))
     if result.failed is True:
         return False
-    result = run(("tar -xf /tmp/" +
-                  "{} -C /data/web_static/releases/{}" +
+    result = run("mkdir -p /data/web_static/releases/{}/"
+                 .format(filename[:-4]))
+    if result.failed is True:
+        return False
+    result = run(("tar -xzf /tmp/" +
+                  "{} -C /data/web_static/releases/{}/" +
                   " --strip-components=1").format(filename, filename[:-4]))
     if result.failed is True:
         return False
     result = run("rm /tmp/{}".format(filename))
     if result.failed is True:
         return False
-    result = run("rm /data/web_static/current")
+    result = run("rm -rf /data/web_static/current")
     if result.failed is True:
         return False
     result = run("ln -s /data/web_static/releases/"
-                 "{} /data/web_static/current".format(filename[:-4]))
+                 "{}/ /data/web_static/current".format(filename[:-4]))
     if result.failed is True:
         return False
-    print("Deployment done for {}".format(env.host))
+    print("New version deployed!")
+    return True
+
 
 
 def deploy():
