331973ac6daa05bc276e41b4a82b120ff5c43ff8
Nicolas Boute <4249@holbertonschool.com>
2022-09-11 22:17:01 +0200

    added strict slashes otpion

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello():
     """Routes '/' to generic html"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Routes '/hbnb' to generic html"""
     return "HBNB"
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text=None):
     """Routes '/c/<text>' to generic html"""
     return f'C {text.replace("_", " ")}'
